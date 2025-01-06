78f06bb5be5ef8193568b9af1f60e52b414787f3
MirasKalaubek <miraskalaubek990@gmail.com>
2025-01-06 16:45:05 +0500

    initial commit

---
+++ src/main/java/kz/solvatech/coffee_for_solva/CoffeeForSolvaApplication.java
@@ -0,0 +1,13 @@
+package kz.solvatech.coffee_for_solva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeForSolvaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeForSolvaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/solvatech/coffee_for_solva/CoffeeForSolvaApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.solvatech.coffee_for_solva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeForSolvaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
